79c49a598802c46bd0fe3572a605a1ff238bf7bb
Muhammed Nour <m.noru.almsto@gmail.com>
2025-10-06 22:59:35 +0300

    firsCommit

---
+++ lib/nour_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NourPage extends StatelessWidget {
+  const NourPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Column(children: [Text('nour screen')])),
+    );
+  }
+}
